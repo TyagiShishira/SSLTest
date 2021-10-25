f15d651b8766a9a0d170cf636502f87f90018d7d
Ayushi Tyagi <ayushi.tyagi@bytemark.co>
2021-10-25 19:42:05 +0530

    First commit

---
+++ SSLTest/SSLTest.h
@@ -0,0 +1,18 @@
+//
+//  SSLTest.h
+//  SSLTest
+//
+//  Created by Ayushi Tyagi on 25/10/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SSLTest.
+FOUNDATION_EXPORT double SSLTestVersionNumber;
+
+//! Project version string for SSLTest.
+FOUNDATION_EXPORT const unsigned char SSLTestVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SSLTest/PublicHeader.h>
+
+
---
+++ SSLTest/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  SSLTest
+//
+//  Created by Ayushi Tyagi on 25/10/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+
+}
